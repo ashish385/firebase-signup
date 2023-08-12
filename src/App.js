4e0b43ff47edd4f688969a8cc1b7462f3248eabf
Ashish Soni <4840asoni@gmail.com>
2023-08-12 10:24:44 +0530

    starter-pack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main class="flex justify-center items-center gap-4 flex-col min-h-screen">
+      <h1 className="text-xl font-bold">Creating Signup page and store data in Firebase.</h1>
+    </main>
+  );
+}
+
+export default App;
